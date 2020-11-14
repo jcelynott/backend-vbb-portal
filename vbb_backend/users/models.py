a715afc9424a99ec03ab17ce5a927dd25bbc07e6
Vignesh Hari <vichuhari100@gmail.com>
2020-11-14 14:58:40 +0530

    Update Some Fields

--- vbb_backend/users/models.py
+++ vbb_backend/users/models.py
@@ -1,19 +1,64 @@
+import enum
+
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class User(AbstractUser):
     """Default user for Village Book Builders Backend."""
 
-    #: Extra User Attributes Are to be added here
+    class UserTypeEnum(enum.Enum):
+        MENTOR = "0"
 
-    def get_absolute_url(self):
-        """Get url for user's detail view.
+    UserTypeChoices = [(e.value, e.name) for e in UserTypeEnum]
+    UserType = models.IntegerField(
+        choices=UserTypeChoices, default=UserTypeEnum.MENTOR.value
+    )
 
-        Returns:
-            str: URL for user detail.
 
-        """
-        return reverse("users:detail", kwargs={"username": self.username})
+class Mentor(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.SET_NULL,
+        related_name="mp",
+        null=True,
+        blank=True,
+        verbose_name=_("User Object of Mentor"),
+    )
+    first_name = models.CharField(
+        max_length=254, null=True, verbose_name=_("First Name")
+    )
+    last_name = models.CharField(max_length=254, null=True, verbose_name=_("Last Name"))
+    personal_email = models.EmailField(
+        null=True, unique=True, verbose_name=_("Personal Email")
+    )
+    vbb_email = models.EmailField(
+        null=True, unique=True, verbose_name=_("Assigned VBB Email")
+    )
+    phone = PhoneNumberField(blank=True, verbose_name=_("Phone Number"))
+    adult = models.CharField(max_length=3, null=True)
+    occupation = models.CharField(
+        max_length=70, null=True, blank=True, verbose_name=_("Occupation")
+    )
+    vbb_chapter = models.CharField(
+        max_length=40, null=True, blank=True, verbose_name=_("VBB Chapter")
+    )
+    affiliation = models.CharField(
+        max_length=70, null=True, blank=True, verbose_name=_("Affiliation")
+    )
+    referral_source = models.CharField(
+        max_length=200, null=True, blank=True, verbose_name=_("Refferal")
+    )
+    languages = models.CharField(max_length=70, null=True, blank=True)
+    time_zone = models.CharField(max_length=40, null=True)
+    charged = models.TextField(max_length=1024, null=True)
+    initials = models.CharField(max_length=6, null=True)
+    desired_involvement = models.CharField(max_length=200, null=True, blank=True)
+    city = models.CharField(max_length=70, null=True, blank=True)
+    advisor_notes = models.TextField(max_length=512, null=True, blank=True)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
