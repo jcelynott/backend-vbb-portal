df0da5d1c8ff9cb7ac73bc7e940932f4f0f48ab3
Vignesh Hari <vichuhari100@gmail.com>
2020-11-29 18:32:07 +0530

    Using Local for production deployment

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="Z67JGjcaJAA5kkvlcIQZIaf7JIq7qSfheIMR8lsbWsKsgxm7UBoE3PH8afTsdzfk",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "*"]
 
 # CACHES
 # ------------------------------------------------------------------------------
